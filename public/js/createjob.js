f1a5eadc05da7932165f5d0fcc7ea123d74958db
Amer Kulovic <amer.kulovic@yahoo.com>
2023-02-17 11:50:43 -0600

    added css styling to other forms and header

--- public/js/createjob.js
+++ public/js/createjob.js
@@ -1,6 +1,11 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
+  const nameEl = document.querySelector('#job-name');
+  const locationEl = document.querySelector('#job-location');
+  const descriptionEl = document.querySelector('#job-desc');
+  const salaryEl = document.querySelector('#job-salary');
+  const benefitsEl = document.querySelector('#job-benefits');
   const name = document.querySelector('#job-name').value.trim();
   const location = document.querySelector('#job-location').value.trim();
   const description = document.querySelector('#job-desc').value.trim();
@@ -20,6 +25,48 @@ const newFormHandler = async (event) => {
       document.location.replace('/profile');
     } else {
       alert('Failed to create job');
+    }
+  } else {
+    if (nameEl.value === '') {
+      nameEl.classList.add('border-dashed', 'border-4', 'border-red-500');
+    } else if (nameEl.value !== '') {
+      nameEl.classList.remove('border-dashed', 'border-4', 'border-red-500');
+    }
+    if (locationEl.value === '') {
+      locationEl.classList.add('border-dashed', 'border-4', 'border-red-500');
+    } else if (locationEl.value !== '') {
+      locationEl.classList.remove(
+        'border-dashed',
+        'border-4',
+        'border-red-500'
+      );
+    }
+    if (descriptionEl.value === '') {
+      descriptionEl.classList.add(
+        'border-dashed',
+        'border-4',
+        'border-red-500'
+      );
+    } else if (descriptionEl.value !== '') {
+      descriptionEl.classList.remove(
+        'border-dashed',
+        'border-4',
+        'border-red-500'
+      );
+    }
+    if (salaryEl.value === '') {
+      salaryEl.classList.add('border-dashed', 'border-4', 'border-red-500');
+    } else if (salaryEl.value !== '') {
+      salaryEl.classList.remove('border-dashed', 'border-4', 'border-red-500');
+    }
+    if (benefitsEl.value === '') {
+      benefitsEl.classList.add('border-dashed', 'border-4', 'border-red-500');
+    } else if (benefitsEl.value !== '') {
+      benefitsEl.classList.remove(
+        'border-dashed',
+        'border-4',
+        'border-red-500'
+      );
     }
   }
 };
